71ba97544d1c3b937761b8d463880884a1d84153
João Lucas Vinente Araújo <joaovinennt@gmail.com>
2026-02-16 12:17:57 -0300

    Tipos de dados de variáveis
    Tipos de dados primitivos de variáveis

---
+++ aula8/index.js
@@ -0,0 +1,12 @@
+const nome = 'joao lucas'; // String
+const idade = 19; // Number
+let nomeAluno;
+const alunos = null;
+let aprovado = true;
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof nomeAluno)
+console.log(typeof alunos)
+console.log(typeof aprovado)
+
