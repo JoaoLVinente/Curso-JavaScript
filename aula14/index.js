3beedd0032bfc932d9f6920edad5481556e6bccd
João Lucas Vinente Araújo <joaovinennt@gmail.com>
2026-02-20 23:58:40 -0300

    Exercícios sobre numbers

---
+++ aula14/index.js
@@ -0,0 +1,12 @@
+let num1 = 12;
+let num2 = 5.36435678322456;
+
+// console.log(num1.toString() + num2)
+// console.log(typeof num1)
+// console.log(num1.toString(2));
+// console.log(num2.toFixed(2));
+// console.log(Number.isInteger(num2)); -> nessa ordem
+
+let temp = num1 * 'ola';
+
+console.log(Number.isNaN(temp));
