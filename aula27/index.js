f659d188ae560b4866523032fbc61e7c01339462
João Lucas Vinente Araújo <joaovinennt@gmail.com>
2026-07-27 16:02:20 -0300

    Estudos sobre operações ternárias

---
+++ aula27/index.js
@@ -0,0 +1,12 @@
+/*
+Operacao ternaria
+? -> para verdadeiro
+: -> para falso
+*/
+
+const pontuacaoUsuario = 99;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+
+console.log(nivelUsuario);
+
+//Isso pode ser usado para substituir um if e else pequeno
