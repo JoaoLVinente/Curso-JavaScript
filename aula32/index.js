ca76df0ba090aa68e41626be9f91841d804a715c
João Lucas Vinente Araújo <joaovinennt@gmail.com>
2026-07-28 17:11:32 -0300

    Atribuição via desestruturação
    Como atribuir via desestruturação
    Como trocar elementos entre si
    Como acessar elementos de um array dentro de outro array

---
+++ aula32/index.js
@@ -0,0 +1,29 @@
+//atribuicao via desestruturacao em arrays
+
+//const frutas = ['banana', 'uva', 'laranja'];
+//const [fruta1, fruta2, fruta3] = frutas;
+
+//console.log(fruta1);
+//console.log(fruta2);
+//console.log(fruta3);
+
+//trocando os valores entre si
+
+//let fruta1 = 'banana';
+//let fruta2 = 'uva';
+//let fruta3 = 'laranja';
+
+//const ordemTrocada = [fruta3, fruta1, fruta2];
+//[fruta1, fruta2, fruta3] = ordemTrocada;
+
+//console.log(fruta1, fruta2, fruta3);
+
+//acessando elementos que estao em um array, dentro de outro array
+
+const alunos = [
+    ['joao', 20],
+    ['maria', 17],
+    ['pedro', 31]
+]
+
+console.log(alunos[0][1]);
